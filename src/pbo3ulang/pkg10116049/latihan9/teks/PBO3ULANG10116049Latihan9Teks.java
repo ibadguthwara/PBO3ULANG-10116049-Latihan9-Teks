a0fab7264838a1173e0989dac79938d551cdc931
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-01-20 23:30:45 +0700

    Initial commit

---
+++ src/pbo3ulang/pkg10116049/latihan9/teks/PBO3ULANG10116049Latihan9Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan9.teks;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA 	     : M. Ibad Guthwara
+ * KELAS	     : PBO 3 ULANG
+ * NIM		     : 10116049
+ * DESKRIPSI PROGRAM : Program ini berisi program untuk teks karakter
+ */
+public class PBO3ULANG10116049Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char C;
+        char tab = '\t';        //buat tab atau biar isi formnya sama rata
+        char newline = '\n';    //buat baris baru sama kayak break / <br>
+        C = 'A';
+        
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + C);
+    }
+    
+}
